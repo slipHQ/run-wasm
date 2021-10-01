2834a3a72390e7454f833b7f2dafafea7bbbdeac
Devon Zara <1634972+devonzara@users.noreply.github.com>
2021-10-01 12:01:21 -0400

    fix: make icons appear on mobile Safari

--- example-nextjs/components/Navbar.tsx
+++ example-nextjs/components/Navbar.tsx
@@ -95,7 +95,7 @@ export default function Navbar({ current }) {
                       'rounded-full self-center flex items-center justify-center h-8 w-8 p-1.5 bg-white shadow-inner border border-gray-100 capitalize'
                     }
                   >
-                    {theme === 'dark' ? (<SunIcon></SunIcon>) : (<MoonIcon></MoonIcon>)}
+                    {theme === 'dark' ? (<SunIcon className="w-full"></SunIcon>) : (<MoonIcon className="w-full"></MoonIcon>)}
                   </button>
                   {/* Mobile menu button */}
                   <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
