62e020033df61facdefa07de2c74e9e04b4307c0
shadab <shadab14meb346@gmail.com>
2021-10-01 15:29:26 +0530

    fix: removed consoles

--- example-nextjs/utils/index.ts
+++ example-nextjs/utils/index.ts
@@ -3,11 +3,9 @@ export const runMethodOnCtrlEnterOrCmdEnterKeyPress = (
   e: any,
   methodToRun: any
 ): void => {
-  console.log(e)
   const ctrlEnter = e.code === 'Enter' && e.ctrlKey
   const cmdEnter = e.code === 'Enter' && e.metaKey
   if (ctrlEnter || cmdEnter) {
-    console.log('kjjkjkjk')
     methodToRun()
   }
 }
