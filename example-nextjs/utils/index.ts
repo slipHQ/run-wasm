8c1077a70495bf0eca2499cb9bbecb5ad399386e
shadab <shadab14meb346@gmail.com>
2021-10-02 01:52:37 +0530

    refactor: remove unused method

--- example-nextjs/utils/index.ts
+++ example-nextjs/utils/index.ts
@@ -1,15 +1,3 @@
-//the argument types here needs to be fixed.
-export const runMethodOnCtrlEnterOrCmdEnterKeyPress = (
-  e: any,
-  methodToRun: any
-): void => {
-  const ctrlEnter = e.code === 'Enter' && e.ctrlKey
-  const cmdEnter = e.code === 'Enter' && e.metaKey
-  if (ctrlEnter || cmdEnter) {
-    methodToRun()
-  }
-}
-
 export interface CustomKeyBinding {
   label: string
   keybinding: any
